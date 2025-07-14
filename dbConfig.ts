66090b5954cf2822059b00088a36284fa4e10c4f
nsshan98 <n.sakibnss@gmail.com>
2025-07-14 22:25:05 +0600

    feat(database): add TypeORM configuration for PostgreSQL connection
    - Integrated TypeORM module into the application by updating app.module.ts to include TypeOrmModule with PostgreSQL configuration.
    - Created a new dbConfig.ts file to define the PostgreSQL connection options, including the connection URL and settings for synchronization.

---
+++ dbConfig.ts
@@ -0,0 +1,9 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+export const pgConfig:PostgresConnectionOptions = {
+    url:'postgresql://neondb_owner:npg_amvPo8uJjw5d@ep-noisy-night-a1sb1xtg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    type: 'postgres',
+    port: 3306,
+    entities: [],
+    synchronize: true,
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { pgConfig } from 'dbConfig';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forRoot(pgConfig)],
   controllers: [AppController],
   providers: [AppService],
 })

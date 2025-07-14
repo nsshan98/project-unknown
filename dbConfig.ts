229bf601c58cc6f496ee0e82bebfd10fd539a858
nsshan98 <n.sakibnss@gmail.com>
2025-07-14 22:51:57 +0600

    feat(entity): create UserEntity with id, name, email, and role fields; update database configuration

--- dbConfig.ts
+++ dbConfig.ts
@@ -1,9 +1,10 @@
+import { UserEntity } from "src/entities/user.entity";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 export const pgConfig:PostgresConnectionOptions = {
     url:'postgresql://neondb_owner:npg_amvPo8uJjw5d@ep-noisy-night-a1sb1xtg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
     type: 'postgres',
     port: 3306,
-    entities: [],
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
 }

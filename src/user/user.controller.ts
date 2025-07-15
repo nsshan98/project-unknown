784840efe9b731ef5bbab5aebe8660318a7ebeee
nsshan98 <n.sakibnss@gmail.com>
2025-07-15 22:44:34 +0600

    feat(user): enhance UserService and UserController with CRUD operations; add UpdateUserDto for user updates

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,35 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query} from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) {}
 
-    @Get()
-    getAllUsers() {
-        return 'All Users'
-    }
-
-    @Get(':id')
-    getUserById(@Param('id', ParseIntPipe) id, @Query('name') name){
-        return `User with ID: ${id} - Name: ${name}`;
-    }
-
-
     @Post()
     createUser(@Body() dto: CreateUserDto) {
         return this.userService.createUser(dto);
     }
+
+    @Get(':id')
+    getUserById(@Param('id', ParseIntPipe) id, @Query('name') name){
+        return this.userService.getSingleUser(id);
+    }
+
+    @Get()
+    getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id', ParseIntPipe) id, @Body() dto: UpdateUserDto) {
+        return this.userService.updateUser(id, dto);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseIntPipe) id) {
+        return this.userService.deleteUser(id);
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
@@ -10,5 +11,30 @@ export class UserService {
 
     async createUser(dto: CreateUserDto){
       return await this.userReporsitory.save(dto);
+    }
+
+    async getSingleUser(id: number){
+        const userInfo =  await this.userReporsitory.findOne({
+            where: {
+                id
+            }
+        })
+        if(!userInfo){
+            throw new NotFoundException()
+        }
+        return userInfo;
+    }
+
+    async getAllUsers() {
+        return await this.userReporsitory.find();
+    }
+
+    async updateUser(id:number, dto:UpdateUserDto){
+        return await this.userReporsitory.update({id}, dto)
+        
+    }
+
+    async deleteUser(id: number){
+        return await this.userReporsitory.delete({id});
     }
 }
